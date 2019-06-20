7d4dbd84d76def1aabd171223658706dc588c73a
AlexStolbov <stolbov_alexey@mail.ru>
2019-06-20 17:35:33 +0300

    fix local changes

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -1,10 +1,9 @@
+
 package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 
 import java.util.Collection;
-import java.util.List;
 
 public interface MealRepository {
     // null if not found, when updated
@@ -15,8 +14,6 @@ public interface MealRepository {
 
     // null if not found
     Meal get(int id);
-
-    List<Meal> getToUser(Integer userId);
 
     Collection<Meal> getAll();
 }
--- src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/inmemory/InMemoryMealRepository.java
@@ -1,16 +1,13 @@
 package ru.javawebinar.topjava.repository.inmemory;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.MealRepository;
 import ru.javawebinar.topjava.util.MealsUtil;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
 
 public class InMemoryMealRepository implements MealRepository {
     private Map<Integer, Meal> repository = new ConcurrentHashMap<>();
@@ -41,13 +38,8 @@ public class InMemoryMealRepository implements MealRepository {
         return repository.get(id);
     }
 
-    public List<Meal> getToUser(Integer userId) {
-        return repository.values().stream().filter(m -> m.getUserId().equals(userId)).collect(Collectors.toList());
-    }
-
     @Override
     public Collection<Meal> getAll() {
         return repository.values();
     }
 }
-
